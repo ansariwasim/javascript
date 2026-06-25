db2be50321bcac58976757ce702ff855e2c82032
ansariwasim <107686270+ansariwasim@users.noreply.github.com>
2026-06-25 10:59:37 +0000

    conversion datatype

---
+++ 01basics/01conversionoperator.js
@@ -0,0 +1,26 @@
+let score = "13";
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber);
+
+//   "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+ let isloggedIn = 0
+
+ let booleanIsLoggedIn = Boolean(isloggedIn)
+//  console.log(booleanIsLoggedIn);
+
+//  1 = true; 0 = false
+//  "" => false
+//  "hitesh" => ture
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
